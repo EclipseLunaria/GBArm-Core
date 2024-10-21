7ea53eddbe7d8587348a02d0f30866e32efcf748
EclipseLunaria <dorsettj@gmail.com>
2024-10-21 09:37:49 -0700

    small changes

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "core.h"
+
+
+int main(){
+    CPU cpu;    
+    init_cpu(&cpu);
+    printf("cpu initialized\n");
+    return 0;
+}
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,8 +4,9 @@ rm -r build
 mkdir build
 cd build
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+
+
 make
 ./run_tests
 
 cd ..
-rm -r build
